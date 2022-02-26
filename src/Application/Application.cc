041ef4f71fc7ef81dd91b8bfe7d3f16b9dbb8d85
ePi <epi5131@gmail.com>
2022-02-26 17:27:35 +0900

    Convert Tab to Space

--- src/Application/Application.cc
+++ src/Application/Application.cc
@@ -15,36 +15,36 @@ Application::Application(HINSTANCE hInstance)
 }
 
 LRESULT CALLBACK Application::wnd_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
-	HDC hdc;
-	PAINTSTRUCT ps;
+  HDC hdc;
+  PAINTSTRUCT ps;
 
-	GetWindowInfo(hwnd, &window_info);
+  GetWindowInfo(hwnd, &window_info);
 
-	tl_drawpos = {
-		0,
-		window_info.rcClient.bottom - window_info.rcClient.top - 240
-	};
+  tl_drawpos = {
+    0,
+    window_info.rcClient.bottom - window_info.rcClient.top - 240
+  };
 
   switch( msg ) {
     case WM_CREATE: {
-			timeline.reset(new Timeline(hwnd));
+      timeline.reset(new Timeline(hwnd));
       break;
     }
 
     case WM_DESTROY: {
-			timeline.reset();
+      timeline.reset();
       PostQuitMessage(0);
       break;
     }
 
-		case WM_PAINT: {
-			hdc = BeginPaint(hwnd, &ps);
-			
-			timeline->draw(hdc, tl_drawpos);
+    case WM_PAINT: {
+      hdc = BeginPaint(hwnd, &ps);
+      
+      timeline->draw(hdc, tl_drawpos);
 
-			EndPaint(hwnd, &ps);
-			break;
-		}
+      EndPaint(hwnd, &ps);
+      break;
+    }
 
     default:
       return DefWindowProc(hwnd, msg, wp, lp);
@@ -55,40 +55,40 @@ LRESULT CALLBACK Application::wnd_proc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp
 
 int Application::main(std::vector<std::wstring> args) {
   HWND hwnd;
-	MSG msg;
-	WNDCLASS winc;
+  MSG msg;
+  WNDCLASS winc;
 
-	winc.style		= CS_HREDRAW | CS_VREDRAW;
-	winc.lpfnWndProc	= wnd_proc_wrap;
-	winc.cbClsExtra	= winc.cbWndExtra	= 0;
-	winc.hInstance		= hInstance;
-	winc.hIcon		= LoadIcon(NULL, IDI_APPLICATION);
-	winc.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	winc.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
-	winc.lpszMenuName	= NULL;
-	winc.lpszClassName	= TEXT("KITTY");
+  winc.style		= CS_HREDRAW | CS_VREDRAW;
+  winc.lpfnWndProc	= wnd_proc_wrap;
+  winc.cbClsExtra	= winc.cbWndExtra	= 0;
+  winc.hInstance		= hInstance;
+  winc.hIcon		= LoadIcon(NULL, IDI_APPLICATION);
+  winc.hCursor		= LoadCursor(NULL, IDC_ARROW);
+  winc.hbrBackground	= (HBRUSH)GetStockObject(WHITE_BRUSH);
+  winc.lpszMenuName	= NULL;
+  winc.lpszClassName	= TEXT("KITTY");
 
-	if( !RegisterClass(&winc) ) {
+  if( !RegisterClass(&winc) ) {
     return -1;
   }
 
-	hwnd = CreateWindow(
+  hwnd = CreateWindow(
     TEXT("KITTY"), TEXT("Kitty on your lap") ,
     WS_OVERLAPPEDWINDOW | WS_VISIBLE,
     CW_USEDEFAULT, CW_USEDEFAULT,
     CW_USEDEFAULT, CW_USEDEFAULT,
     NULL, NULL,
     hInstance, NULL
-	);
+  );
 
-	if( hwnd == NULL ) {
+  if( hwnd == NULL ) {
     return -1;
   }
 
-	while( GetMessage(&msg, NULL, 0, 0) ) {
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
+  while( GetMessage(&msg, NULL, 0, 0) ) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
   
-	return msg.wParam;
+  return msg.wParam;
 }
